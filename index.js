a024285b2615b03ae816af9803015b480b7efa85
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2021-06-10 15:52:19 +0200

    up

---
+++ index.js
@@ -0,0 +1,22 @@
+const openBtn = document.getElementById("hamburger")
+const closeBtn = document.getElementById("cross")
+const navBar = document.getElementById("navModal")
+// const contentContainer = document.getElementById(".container")
+
+openBtn.addEventListener("click", toggle)
+closeBtn.addEventListener("click", toggle)
+
+function toggle() {
+    navBar.classList.toggle("active-nav")
+    console.log("hello")
+    // contentContainer.classList.toggle("inactive-container")
+}
+
+// contentContainer.addEventListener("click", close)
+
+function close(event) {
+    if (!event.target.classList.contains("material-icons-outlined")) {
+        navBar.classList.remove("active-nav")
+        // contentContainer.classList.remove("inactive-container")
+    }
+}
